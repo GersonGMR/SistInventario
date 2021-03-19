e5a1bad1ff8da7bc68e873bf229fb049dc715ff6
GersonGMR <56897299+GersonGMR@users.noreply.github.com>
2021-03-19 14:16:07 -0600

    Importando proyecto

---
+++ includes/config.php
@@ -0,0 +1,24 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| OWSA-INV V2
+|--------------------------------------------------------------------------
+| Author: Siamon Hasan
+| Project Name: OSWA-INV
+| Version: v2
+| Official page: http://oswapp.com/
+| facebook Page: https://www.facebook.com/oswapp
+|
+|
+|
+*/
+define( 'DB_HOST', 'localhost' );			// Set database host
+//define( 'DB_USER', 'root' );			// Set database user
+//define( 'DB_PASS', 'root' );			// Set database password
+define( 'DB_USER', 'root' );		// Set database user
+define( 'DB_PASS', '' );				// Set database password
+define( 'DB_NAME', 'oswa_inv' );			// Set database name
+
+session_write_close( );
+session_start();
+?>
---
+++ mytests/test1.php
@@ -0,0 +1,9 @@
+<?php
+
+session_start();
+
+echo "DIRECTORY_SEPARATOR is: " . DIRECTORY_SEPARATOR . "\n";
+
+phpinfo();
+
+?>
