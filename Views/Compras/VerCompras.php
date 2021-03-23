3a4a97b865dee0c82a6f28c0e5fc0011913aa395
GersonGMR <56897299+GersonGMR@users.noreply.github.com>
2021-03-23 16:56:10 -0600

    reestructurando el proyecto con codeigniter, mvc, ajax, php y mysql

---
+++ Views/Compras/VerCompras.php
@@ -0,0 +1,43 @@
+<?php
+require_once "Assets/pdf/fpdf.php";
+$total = 0.00;
+$pdf = new FPDF('P', 'mm', array(105, 148));
+$pdf->AddPage();
+$pdf->setFont('Arial', 'B', 14);
+$pdf->setTitle("Reporte de entrada");
+$pdf->image(base_url().'Assets/img/logo.jpg', 70, 5, 30, 20, 'JPG');
+$pdf->setFont('Arial', 'B', 10);
+$pdf->Cell(50, 5, utf8_decode($alert['nombre']), 0, 1, 'L');
+$pdf->Cell(20, 5, utf8_decode("Ruc"), 0, 0, 'L');
+$pdf->setFont('Arial', '', 10);
+$pdf->Cell(50, 5, utf8_decode($alert['ruc']), 0, 1, 'L');
+$pdf->setFont('Arial', 'B', 10);
+$pdf->Cell(20, 5, utf8_decode("Teléfono"), 0, 0, 'L');
+$pdf->setFont('Arial', '', 10);
+$pdf->Cell(50, 5, utf8_decode($alert['telefono']), 0, 1, 'L');
+$pdf->setFont('Arial', 'B', 10);
+$pdf->Cell(20, 5, utf8_decode("Dirección"), 0, 0, 'L');
+$pdf->setFont('Arial', '', 10);
+$pdf->Cell(50, 5, utf8_decode($alert['direccion']), 0, 1, 'L');
+$pdf->Ln();
+$pdf->setFont('Arial', 'B', 10);
+$pdf->Cell(80, 8, utf8_decode("Detalle de los Productos"), 0, 1, 'C');
+$pdf->setFont('Arial', 'B', 10);
+$pdf->SetTextColor(255, 255, 255);
+$pdf->Cell(35, 5, utf8_decode('Descripción'), 1, 0, 'C', 1);
+$pdf->Cell(10, 5, 'Cant.', 1, 0, 'C', 1);
+$pdf->Cell(40, 5, 'Fecha', 1, 1, 'C', 1);
+
+foreach ($data as $compras) {
+    $subtotal = $compras['cantidad'];
+    $total = $total + $subtotal;
+    $pdf->SetTextColor(0, 0, 0);
+    $pdf->Cell(35, 5, utf8_decode($compras['producto']), 1, 0, 'L');
+    $pdf->Cell(10, 5, $compras['cantidad'], 1, 0, 'L');
+    $pdf->Cell(40, 5, $compras['fecha'], 1, 1, 'L');
+}
+$pdf->Ln();
+$pdf->Cell(90, 5, 'Total Unidades: '. number_format( $total), 0, 1, 'R');
+
+$pdf->Output();
+?>
--- index.php
+++ index.php
@@ -1,27 +1,24 @@
 <?php
-  ob_start();
-  require_once('includes/load.php');
-  if($session->isUserLoggedIn(true)) { redirect('home.php', false);}
+require_once("Config/Config.php");
+require_once("Helpers/Helpers.php");
+$url = isset($_GET['url']) ? $_GET['url'] : "Home/home";
+$arrUrl = explode("/", $url);
+$controller = $arrUrl[0];
+$methop = $arrUrl[0];
+$params = "";
+if (isset($arrUrl[1])) {
+    if ($arrUrl[1] != "") {
+        $methop = $arrUrl[1];
+    }
+}
+if (isset($arrUrl[2])) {
+    if ($arrUrl[2] != "") {
+        for ($i=2; $i < count($arrUrl); $i++) { 
+            $params .= $arrUrl[$i]. ',';
+        }
+        $params = trim($params, ',');
+    }
+}
+require_once("Libraries/Core/Autoload.php");
+require_once("Libraries/Core/Load.php");
 ?>
-<?php include_once('layouts/header.php'); ?>
-<div class="login-page">
-    <div class="text-center">
-       <h1>Bienvenido</h1>
-       <p>Iniciar sesión </p>
-     </div>
-     <?php echo display_msg($msg); ?>
-      <form method="post" action="auth.php" class="clearfix">
-        <div class="form-group">
-              <label for="username" class="control-label">Usario</label>
-              <input type="name" class="form-control" name="username" placeholder="Usario">
-        </div>
-        <div class="form-group">
-            <label for="password" class="control-label">Contraseña</label>
-            <input type="password" name= "password" class="form-control" placeholder="Contraseña">
-        </div>
-        <div class="form-group">
-                <button type="submit" class="btn btn-info  pull-right">Entrar</button>
-        </div>
-    </form>
-</div>
-<?php include_once('layouts/footer.php'); ?>
