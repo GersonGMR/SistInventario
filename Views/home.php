922b25c0f0a3331e14e04bf2be199db4ce4e0c32
Miltioxs <miltioxs98@hotmail.com>
2021-05-10 20:33:46 -0600

    formulario del login validacion funcionando

--- Views/home.php
+++ Views/home.php
@@ -6,6 +6,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link rel="stylesheet" href="<?php echo base_url(); ?>Assets/css/bootstrap.min.css">
+
+    <!--
+        <style>
+        input:invalid {
+            box-shadow: 0 0 5px 1px red;;
+        }
+        input:valid {
+        border: 2px solid black;
+        }
+    </style>
+        -->
+        
 </head>
 
 <body class="bg-secondary">
@@ -23,7 +35,10 @@
                                 <strong>Usuario o contraseña Incorrecta</strong>
                             </div>
                         <?php } ?>
-                        <form action="<?php echo base_url(); ?>Usuarios/login" method="post" autocomplete="off">
+                        
+                        <div id ='error' class="text-danger"></div>
+                        <form action="<?php echo base_url(); ?>Usuarios/login" method="post" autocomplete="off" onsubmit="return validar()">
+                     
                             <div class="form-group">
                                 <strong class="text-primary">Usuario</strong>
                                 <input id="usuario" class="form-control" type="text" name="usuario" placeholder="Usuario">
@@ -32,13 +47,51 @@
                                 <strong class="text-primary">Contraseña</strong>
                                 <input id="clave" class="form-control" type="password" name="clave" placeholder="Contraseña">
                             </div>
+                            <input class="btn btn-primary btn-block" type="submit" value="Iniciar" >
+                            <!--
                             <button class="btn btn-primary btn-block" type="submit">Iniciar</button>
+                            -->
                         </form>
+                        
                     </div>
                 </div>
             </div>
         </div>
     </div>
+                        <script>
+                            function limpiarFormulario(){
+                                var error = document.getElementById('error');
+                                error.innerHTML = "";
+                            }
+                            
+                            function validar(){
+                                //limpiarFormulario();
+                                var usuario = document.getElementById('usuario');
+                                var clave = document.getElementById('clave');
+                                var error = document.getElementById('error');
+                                //error.style.color = 'red';
+                                var return_value = true;
+
+                                console.log('enviando formulario');
+                                var mensajesError = [];
+
+                                if (usuario.value === null || usuario.value=== ''){
+                                    mensajesError.push('Ingresa tu usuario');
+                                    return_value = false;
+                                }
+
+                                if (clave.value === null || clave.value=== ''){
+                                    mensajesError.push('Ingresa tu Contraseña');
+                                    return_value = false;
+                                }
+
+                                error.innerHTML = mensajesError.join(', ');
+
+                                
+                                return return_value;
+                            }
+                            
+                        </script>
 </body>
 
 </html>
