12d69fae55dae4b8c7cf91b398ab714886853b3f
GersonGMR <56897299+GersonGMR@users.noreply.github.com>
2021-06-11 11:57:39 -0600

    Color modificado en cards del index.

--- Views/Admin/Listar.php
+++ Views/Admin/Listar.php
@@ -30,7 +30,7 @@
 
                 <!-- Earnings (Monthly) Card Example -->
                 <div class="col-xl-3 col-md-6 mb-4">
-                    <div class="card bg-secondary border-0 shadow h-100 py-2">
+                    <div class="card bg-info border-0 shadow h-100 py-2">
                         <div class="card-body">
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2">
